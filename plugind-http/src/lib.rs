05879a7fe615793d591ee94f07260b4685145e22
viniciusfonseca <vfonseca1618@gmail.com>
2025-10-19 13:58:20 -0300

    add json error

--- plugind-http/src/lib.rs
+++ plugind-http/src/lib.rs
@@ -1,3 +1,5 @@
+use serde_json::json;
+
 pub struct Json;
 
 impl Json {
@@ -10,5 +12,16 @@ impl Json {
         buf.extend_from_slice(format!("Content-Length: {}\r\n\r\n", body.len()).as_bytes());
         buf.extend_from_slice(&body);
         Ok(buf)
+    }
+
+    pub fn error(status: u16, message: &str) -> anyhow::Error {
+        let mut buf = String::new();
+        let body = json!({ "message": message });
+        let body = serde_json::to_string(&body).unwrap();
+        buf.push_str(&format!("HTTP/1.1 {}\r\n", status));
+        buf.push_str("Content-Type: application/json\r\n");
+        buf.push_str(&format!("Content-Length: {}\r\n\r\n", body.len()));
+        buf.push_str(&body);
+        anyhow::Error::msg(buf)
     }
 }
